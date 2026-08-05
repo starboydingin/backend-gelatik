27859de071fdd85ddd32e51a60dba6ef2107b8fc
starboydingin <adwikafa@gmail.com>
2026-08-05 13:10:48 +0700

    chore(backend): remove verified duplicate Laravel source tree

--- scratch/check_dup_json.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$json = json_decode(file_get_contents(__DIR__ . '/dump_dup_analysis.json'), true);
-foreach ($json as $table => $data) {
-    if ($data['duplicate_id_count'] > 0) {
-        echo "Table: $table, Dup Groups: " . json_encode($data['dup_groups']) . "\n";
-    }
-}
--- scratch/inspect_activity_log_dump.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$json = json_decode(file_get_contents(__DIR__ . '/dump_dup_analysis.json'), true);
-if (isset($json['activity_log']['dup_groups'][1])) {
-    echo "=== ACTIVITY_LOG ID 1 IN DUMP ===\n";
-    foreach ($json['activity_log']['dup_groups'][1]['rows'] as $idx => $r) {
-        echo "Row #" . ($idx + 1) . ": " . json_encode($r) . "\n";
-    }
-}
